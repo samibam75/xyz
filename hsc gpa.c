c364105bf2d713af7c0b47842299f12e1f6eb2b6
samibam75 <152017333+samibam75@users.noreply.github.com>
2023-11-26 01:47:24 +0600

    Add files via upload
    commit

---
+++ hsc gpa.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+printf("Hi dear\n");
+printf("I hope you are doing well\n");
+printf("I know your result have been published\n");
+
+printf("Let's celebrate it together\n");
+printf("Share your GPA\n");
+
+
+
+float a;
+printf("Enter your GPA:\n");
+scanf("%f",&a);
+if(a>=5){
+    printf("Congratulations.... dear\n");
+
+printf("Now is  the time of celebration.Celebrate your result with your parents,relatives& friends.\n");
+printf("Remember,you never get back this time back,so try to enjoy & celebrate this\n");
+   printf("Best of luck for your next admission journey\n");
+
+}
+
+ else if(a<5){
+     printf("Congratulations.... dear\n");
+     printf("Don't lose your hope.It's just a bad chapter of your life journey\n");
+    printf("Try to find out all possibilities and scope which is open for you\n");
+    printf("Best of luck for your next admission journey\n");
+
+}
+
+printf("Thank you dear");
+}
